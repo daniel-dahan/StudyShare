5e1b9fbf69dcbbd3faf9a73520063af9888931ba
fabulouse <birba.fabien@gmail.com>
2016-02-25 17:37:21 +0100

    re
    debug

--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -8,53 +8,41 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/")
      */
     public function indexAction()
     {
-        return $this->render('UserBundle:Default:loging.html.twig');
+        return $this->render('UserBundle:Studyview:login.html.twig');
     }
+
     /**
      * @Route("/home", name="home")
      */
     public function homeAction()
     {
-<<<<<<< HEAD
-        return $this->render('UserBundle:Studyview:index.html.twig');
-=======
         return $this->render('UserBundle:Studyview:student.html.twig');
->>>>>>> origin/master
     }
+
     /**
      * @Route("/student", name="student")
      */
-<<<<<<< HEAD
-    public function teacherfAction()
-    {
-        return $this->render('UserBundle:Studyview:index.html.twig');
-=======
 
     public function studentAction()
 
     {
-        return $this->render('UserBundle:Studyview:teac.html.twig');
->>>>>>> origin/master
+        return $this->render('UserBundle:Studyview:student.html.twig');
     }
+
     /**
      * @Route("/teacher", name="teacher")
      */
-<<<<<<< HEAD
-    public function studentAction()
-    {
-        return $this->render('UserBundle:Studyview:index-prof.html.twig');
-=======
 
     public function teachertAction()
 
     {
         return $this->render('UserBundle:Studyview:teacher.html.twig');
->>>>>>> origin/master
     }
+
     /**
      * @Route("/post_subject", name="post_subject")
      */
@@ -62,9 +50,4 @@ class DefaultController extends Controller
     {
         return $this->render('UserBundle:Studyview:add-cour.html.twig');
     }
-<<<<<<< HEAD
 }
-=======
-
-}
->>>>>>> origin/master
