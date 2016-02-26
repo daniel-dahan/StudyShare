e9ccd0dfd57e550dbf67e5ea795069619062551d
fabulouse <birba.fabien@gmail.com>
2016-02-26 11:17:47 +0100

    page rajoutée
    10 Page dans le studyview
    plus une « register » dans le dossier form de UserBundle

--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -10,7 +10,7 @@ class DefaultController extends Controller
     /**
      * @Route("/")
      */
-    public function indexAction()
+    public function logInAction()
     {
         return $this->render('UserBundle:Studyview:login.html.twig');
     }
@@ -34,11 +34,43 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/post_subject", name="post_subject")
+     * @Route("/subject_add", name="subject_add")
      */
-    public function postSubjectAction()
+    public function subjectAddAction()
     {
         return $this->render('UserBundle:Studyview:add-cour.html.twig');
+    }
+
+    /**
+     * @Route("/subject_manage", name="subject_manage")
+     */
+    public function subjectManageAction()
+    {
+        return $this->render('UserBundle:Studyview:manage-cour.html.twig');
+    }
+
+    /**
+     * @Route("/pwd_forgotten", name="pwd_forgotten")
+     */
+    public function pwdForgottenAction()
+    {
+        return $this->render('UserBundle:Studyview:password-forgotten.html.twig');
+    }
+
+    /**
+     * @Route("/pwd_change", name="pwd_change")
+     */
+    public function pwdChangeAction()
+    {
+        return $this->render('UserBundle:Studyview:modify-password.html.twig');
+    }
+
+    /**
+     * @Route("/subject_change", name="subject_change")
+     */
+    public function pwdChangeAction()
+    {
+        return $this->render('UserBundle:Studyview:view-cour.html.twig');
     }
 
     /**
@@ -46,14 +78,22 @@ class DefaultController extends Controller
      */
     public function registerAction($admin)
     {
-        return $this->render('UserBundle:Studyview:add-cour.html.twig');
+        return $this->render('UserBundle:Form:RegistrationFormType.php');
     }
 
     /**
-     * @Route("/register/{admin}", name="register")
+     * @Route("/subject_list", name="subject_list")
      */
-    public function addCommAction()
+    public function subjectListAction()
     {
-        return $this->render('UserBundle:Studyview:add-cour.html.twig');
+        return $this->render('UserBundle:Studyview:list-cours.html.twig');
+    }
+
+    /**
+     * @Route("/subject_list", name="subject_list")
+     */
+    public function subjectAction()
+    {
+        return $this->render('UserBundle:Studyview:list-cours.html.twig');
     }
 }
